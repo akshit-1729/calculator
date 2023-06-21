7e3e8f94e5f0eda8836150e90b84d7fe244058b8
Srihari <srihari100499@gmail.com>
2023-06-21 11:02:32 +0530

    Basic working model

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-function callFunc()
-{
-    document.getElementById("display").innerHTML="Clicked"
-}
+window.addEventListener("click", function (event) {
+
+    let clickedElement = event.target;
+    let output = document.getElementById("display")
+
+    let isDiv = clickedElement instanceof HTMLDivElement;
+    if (isDiv) {
+        if(clickedElement.innerHTML=="=")
+        {
+            try {
+                output.innerHTML=eval(output.innerHTML);
+            } catch (error) {
+                output.innerHTML="Error " + error;
+            }
+        }
+        else if(clickedElement.innerHTML=="CE")  
+        {
+            output.innerHTML=""
+        }
+        else
+        {
+            output.innerHTML += clickedElement.innerHTML;
+        }
+    }
+});
